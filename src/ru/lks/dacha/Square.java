c7a9f43e2580d9a1de70f35b48ff680f7b47e4b4
Constantine <k.lantratov@gmail.com>
2014-02-08 18:32:03 +0400

    Squere desc

--- src/ru/lks/dacha/Square.java
+++ src/ru/lks/dacha/Square.java
@@ -1,15 +1,30 @@
 package ru.lks.dacha;
 
+
+/**
+ * Класс для реализации меры площади. Служит для задания площади объектов (дома, фундамента и т.д.)
+ * @author Constantine
+ *
+ */
 public class Square {
 	
 	private float width;
 	private float length;
 	private float square;
 	
+	/**
+	 * конструктор. для инициализации используется площадь в м2
+	 * @param square
+	 */
 	public Square (float square){
 		this.square = square;
 	}
 	
+	/**
+	 * конструктор. для инициализации используются длина и ширина
+	 * @param width
+	 * @param lenght
+	 */
 	public Square (float width, float lenght){
 		square = width*lenght;
 	}
